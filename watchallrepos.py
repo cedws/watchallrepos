143bc8d4ecdc3741803acecc03969691820fca9f
Connor E <38229097+c-edw@users.noreply.github.com>
2019-04-19 21:41:12 +0100

    Reindent and apply lints from pylint.

--- watchallrepos.py
+++ watchallrepos.py
@@ -3,49 +3,50 @@
 import os
 import asyncio
 
-from aiohttp import ClientSession
 from argparse import ArgumentParser
+from aiohttp import ClientSession
 
 API = "https://api.github.com"
 
-parser = ArgumentParser()
-parser.add_argument(
-	"-s", "--skip_forks", 
-	action="store_true", 
-	help="skip your repositories which are forks"
+PARSER = ArgumentParser()
+PARSER.add_argument(
+    "-s", "--skip_forks",
+    action="store_true",
+    help="skip your repositories which are forks"
 )
 
 async def main():
-	token = os.environ.get("GITHUB_PA_TOKEN")
+    token = os.environ.get("GITHUB_PA_TOKEN")
 
-	if not token:
-		print(
-			"You must pass a GitHub personal access token via the "
-			"GITHUB_PA_TOKEN environment variable."
-		)
-		return
+    if not token:
+        print(
+            "You must pass a GitHub personal access token via the "
+            "GITHUB_PA_TOKEN environment variable."
+        )
+        return
 
-	args = parser.parse_args()
+    args = PARSER.parse_args()
+    headers = {
+        "Authorization": "Token {}".format(token)
+    }
 
-	async with ClientSession(headers={
-		"Authorization": "token {}".format(token)
-	}) as client:
-		repositories = await repos(client)
+    async with ClientSession(headers=headers) as client:
+        repositories = await repos(client)
 
-		for repo in repositories:
-			if args.skip_forks and repo.get("fork"):
-				continue
+        for repo in repositories:
+            if args.skip_forks and repo.get("fork"):
+                continue
 
-			print("Watching {}".format(repo.get("url")))
-			await subscribe(client, repo)
-				
+            print("Watching {}".format(repo.get("url")))
+            await subscribe(client, repo)
+
 async def repos(client):
-	async with client.get("{}/user/repos".format(API)) as resp:
-		return await resp.json()
+    async with client.get("{}/user/repos".format(API)) as resp:
+        return await resp.json()
 
 async def subscribe(client, repo):
-	await client.put(repo.get("subscription_url"))
+    await client.put(repo.get("subscription_url"))
 
 if __name__ == "__main__":
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(main())
+    LOOP = asyncio.get_event_loop()
+    LOOP.run_until_complete(main())
